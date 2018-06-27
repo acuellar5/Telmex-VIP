45d3ba9c07e1f42a3fba32f990dca2e30329ecbe
bredybuitrago <bredybuitrago@gmail.com>
2018-06-27 08:31:07 -0500

    select

--- assets/js/modules/moduleOts.js
+++ assets/js/modules/moduleOts.js
@@ -321,6 +321,7 @@ $(function () {
         }
     };
     quinceDias.init();
+    //******************************************************** FIN 15 DIAS ********************************************************************//
 
         eventos = {
             init: function () {
@@ -362,13 +363,12 @@ $(function () {
                         break;                
                 }
                 eventos.fillFormModal(record);
-                // console.log(record);
+                 // console.log(record);
             },
 
             //llenamos los input del modal con la informacion a la q le dio click
             fillFormModal: function(registro){
-                console.log("registro", registro);
-
+                console.log(registro);
                 // limpiar el formulario...
                 document.getElementById('formModal').reset();
                 $('#k_id_estado_ot').html("");
@@ -377,11 +377,16 @@ $(function () {
                     $('#' + i).val(item);
                 }); 
 
-                eventos.fillSelect(registro.k_id_tipo);
+                eventos.fillSelect(registro.k_id_tipo, registro.k_id_estado_ot);
+
+                console.log(registro.k_id_estado_ot);
+                // $('#k_id_estado_ot option[value="'+registro.k_id_estado_ot+'"]').attr('selected', true);
+                // $(`#k_id_estado_ot option [value= "${registro.k_id_estado_ot}"]`).attr("selected", true);
+
                 $('#modalEditTicket').modal('show');
             },
 
-            fillSelect: function(idtipo){
+            fillSelect: function(idtipo, val_estado){
               $.post(baseurl + "/User/c_getStatusByType",
                   {
                     idtipo: idtipo
@@ -391,8 +396,12 @@ $(function () {
                     var status = JSON.parse(data);
                     // Pinto el select de estado
                     $.each(status,function(i,item){
-                      $('.llenarEstadosJS').append('<option value="'+item.k_id_estado_ot+'">'+item.n_name_estado_ot+'</option>');
-                    });                
+                        if (val_estado == item.k_id_estado_ot) {
+                            $('.llenarEstadosJS').append('<option value="'+item.k_id_estado_ot+'" selected>'+item.n_name_estado_ot+'</option>');                            
+                        }else {
+                            $('.llenarEstadosJS').append('<option value="'+item.k_id_estado_ot+'">'+item.n_name_estado_ot+'</option>');
+                        }
+                    }); 
                 });
 
             },
