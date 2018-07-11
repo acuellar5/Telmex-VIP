54ebf1157fdd7c58d4ba8f1255b3f0a4fdde25b1
Admin <Admin@172.19.193.206>
2018-07-11 10:21:45 -0500

    correcion de error modal

--- application/views/parts/footerF.php
+++ application/views/parts/footerF.php
@@ -53,9 +53,6 @@
 
 
 <!-- ***********************faber*********************************** -->
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
