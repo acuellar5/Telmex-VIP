333553a6bbd25099462a5e805b974ae3fd79ec78
bredybuitrago <bredybuitrago@gmail.com>
2018-07-12 14:25:33 -0500

    nuevos tipos 100%

--- application/controllers/Type.php
+++ application/controllers/Type.php
@@ -71,48 +71,58 @@ class Type extends CI_Controller {
 
   // guardar variantes nuevas de Tipos
   public function c_save_type_variant(){
-    // Creo los valores para insertar la variante en tipo_ot
-    $data = array(
-      'i_referencia'   => $this->input->post('id_type'),
-      'n_name_tipo' => $this->input->post('name')
-    );
-    // inserto el nuevo registro 
-    $id_tipo_nuevo = $this->Dao_tipo_ot_hija_model->insert_new_type($data);
-    // traigo los registros que tengan el nombre del tipo nuevo
-    $registros     = $this->Dao_ot_hija_model->get_ot_by_tipo($this->input->post('name'));
-    //contadores de actualizados y actualizados nulos
-    $act   = 0;
-    $nulos = 0;
-    // Recorro los registros que vengan con el tipo nuevo
-    for ($i=0; $i < count($registros); $i++) { 
-      // Obtengo el id del estado con la combinacion de tipo y estado del registro
-      $id_estado = $this->Dao_estado_ot_model->get_status_by_idtipo_and_name_status($this->input->post('id_type'), $registros[$i]->estado_orden_trabajo_hija);
-      // si existe ese estado...
-      if ($id_estado) {
-        // Creo los arreglo para la actualizacion de los registros 
-        $up = array(
-          'k_id_register'  => $registros[$i]->k_id_register,
-          'k_id_estado_ot' => $id_estado->k_id_estado_ot
+    // vERIFICO QUE EL TIPO NI EXISTA
+    $exis = $this->Dao_tipo_ot_hija_model->get_tipo_ot_hija_by_name($this->input->post('name'));
+    if (!$exis) {
+        // Creo los valores para insertar la variante en tipo_ot
+        $data = array(
+          'i_referencia'   => $this->input->post('id_type'),
+          'n_name_tipo' => $this->input->post('name')
         );
-        $res = $this->Dao_ot_hija_model->update_ot_hija($up);
-        // Si se actulizó el registro suma un uno al contador
-        $act++;
-      } else {
-        // actualizar el k_id_estado a null
-        $up_null = array(
-          'k_id_register'  => $registros[$i]->k_id_register,
-          'k_id_estado_ot' => null
-        );
-        $res = $this->Dao_ot_hija_model->update_ot_hija($up_null);
-        $nulos++;
-      }
+        // inserto el nuevo registro 
+        $id_tipo_nuevo = $this->Dao_tipo_ot_hija_model->insert_new_type($data);
+        // traigo los registros que tengan el nombre del tipo nuevo
+        $registros     = $this->Dao_ot_hija_model->get_ot_by_tipo($this->input->post('name'));
+        //contadores de actualizados y actualizados nulos
+        $act   = 0;
+        $nulos = 0;
+        // Recorro los registros que vengan con el tipo nuevo
+        for ($i=0; $i < count($registros); $i++) { 
+          // Obtengo el id del estado con la combinacion de tipo y estado del registro
+          $id_estado = $this->Dao_estado_ot_model->get_status_by_idtipo_and_name_status($this->input->post('id_type'), $registros[$i]->estado_orden_trabajo_hija);
+          // si existe ese estado...
+          if ($id_estado) {
+            // Creo los arreglo para la actualizacion de los registros 
+            $up = array(
+              'k_id_register'  => $registros[$i]->k_id_register,
+              'k_id_estado_ot' => $id_estado->k_id_estado_ot
+            );
+            $res = $this->Dao_ot_hija_model->update_ot_hija($up);
+            // Si se actulizó el registro suma un uno al contador
+            $act++;
+          } else {
+            // actualizar el k_id_estado a null
+            $up_null = array(
+              'k_id_register'  => $registros[$i]->k_id_register,
+              'k_id_estado_ot' => null
+            );
+            $res = $this->Dao_ot_hija_model->update_ot_hija($up_null);
+            $nulos++;
+          }
 
+        }
+        // Array para retornar los registros afectados
+        $respuesta = array(
+          'actu' => $act,
+          'nulos' => $nulos
+        );
+    } else {
+        // Si ya existe el tipo retorno 0 y no hace nada
+        $respuesta = array(
+            'actu' => 0,
+            'nulos' => 0
+          );
     }
-    // Array para retornar los registros afectados
-    $respuesta = array(
-      'actu' => $act,
-      'nulos' => $nulos
-    );
     echo json_encode($respuesta);
   }
 
