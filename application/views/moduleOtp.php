58dc8ee36cf77f844f3611d983ff9655bd2499b3
bredybuitrago <bredybuitrago@gmail.com>
2018-07-25 17:14:01 -0500

    MERGE

--- application/views/moduleOtp.php
+++ application/views/moduleOtp.php
@@ -4,7 +4,7 @@
 	<?php 
 		for ($i=0; $i < count($ingenieros); $i++) { 
 		    echo "<button class='accordion' data-iduser='".$ingenieros[$i]->k_id_user."'>".$ingenieros[$i]->nombre." <img src='".URL::base()."/assets/images/plus.png' class='rigth'> <a class='rigth btn btn-default' target='_blank' href='".URL::base()."/OtHija/detalle/".$ingenieros[$i]->k_id_user."'><span class='glyphicon glyphicon-eye-open' title='ver detalle'></span></a> <a class='rigth btn btn-default' target='_blank' href='".URL::base()."/OtHija/exportar/".$ingenieros[$i]->k_id_user."'><span class='glyphicon glyphicon-export' title='exportar a excel'></span></a>
-				<h4 align='center' class='h4_for'><span class='badge all'>22</span>  <span class='badge in_time'>45</span>  <span class='badge out_time'>22</span>  <span class='badge today'>12</span></h4><br>	
+				
 					
 
 		    </button>";
