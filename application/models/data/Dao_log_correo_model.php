7586ee8a44ab83b3320656fc3ce5c0fb0edee81d
bredybuitrago <bredybuitrago@gmail.com>
2018-08-02 10:59:53 -0500

    log_mail modal 89%

--- application/models/data/Dao_log_correo_model.php
+++ application/models/data/Dao_log_correo_model.php
@@ -15,6 +15,13 @@ class Dao_log_correo_model extends CI_Model {
     	$this->db->insert('log_correo', $data);
     }
 
+    // Obtiene datos de log_correo por id ot hija
+    public function getLogMailById($id){
+    	// $query = $this->db->order_by('k_id_tipo ASC, i_orden ASC');
+    	$query = $this->db->get_where('log_correo', array('id_orden_trabajo_hija' => $id));
+    	return $query->result();
+    }
+
 
 
 
