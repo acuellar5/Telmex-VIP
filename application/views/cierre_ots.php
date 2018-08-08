cd0e7454a2c1cb0b0b6179f408bef45eeedd8f40
bredybuitrago <bredybuitrago@gmail.com>
2018-08-08 09:37:03 -0500

    enrutar 88%

--- application/views/cierre_ots.php
+++ application/views/cierre_ots.php
@@ -1,3 +1,4 @@
+<script src="<?= URL::to("assets/plugins/sweetalert2/sweetalert2.all.js") ?>"></script>
 <h3>Enrutamiento OTS</h3>
 <table id="tables_cierre" class="table table-hover table-bordered table-striped dataTable_camilo" width="100%">
     <tfoot>
@@ -17,3 +18,23 @@
     </tr>
     </tfoot>
 </table>
+
+<!------------------------------------------ MODAL DE ORDENES SELECCIONADAS PARA CIERRE ------------------------------------------>
+<div id="mdl_cierre" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">X</button>
+                <h3 class="modal-title" id="mdl-title-cierre">ORDENES SELECCIONADAS</h3>
+            </div>
+            <div class="modal-body">
+                <table id="table_selected" class="table table-hover table-bordered table-striped dataTable_camilo" width="100%"></table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><i class='glyphicon glyphicon-remove'></i>&nbsp;Cancelar</button>
+                <button type="button" class="btn btn-success" id="mdl-cierre-send"><i class='glyphicon glyphicon-send'></i>&nbsp;enviar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!---------------------------------------- FIN MODAL DE ORDENES SELECCIONADAS PARA CIERRE-------------------------------------- -->
