24cdffa2e68905e946bf779713551025690c22bb
acuellar5 <acuellar_5@hotmail.com>
2018-08-29 08:51:01 -0500

    Integracion codigo Jhon

--- application/views/contolDeCambios.php
+++ application/views/contolDeCambios.php
@@ -2,6 +2,7 @@
 <ul class="nav nav-tabs">
     <li class="active"><a data-toggle="tab" href="#track_changes_office">Sedes</a></li>
     <li class=""><a data-toggle="tab" href="#track_changes_OTP">OTP</a></li>
+    <li class=""><a data-toggle="tab" href="#track_changes_OTPAll">Control de Cambios</a></li>
 </ul>
 
 
@@ -25,10 +26,29 @@
             </tfoot>
         </table>
     </div>
+
     <!--*********************  Contendio de la pestaña de OTP *********************-->
     <div id="track_changes_OTP" class="tab-pane fade">
         <h3>OTP</h3>
         <table id="trackChanges_OTP" class="table table-hover table-bordered table-striped dataTable_camilo" width="100%">
+            <tfoot>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th class="tamanoColumOpc"></th>                    
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+
+    <!--*********************  Contendio de la pestaña de Control de Cambio *********************-->
+    <div id="track_changes_OTPAll" class="tab-pane fade">
+        <h3>OTP</h3>
+        <table id="trackChangesAll" class="table table-hover table-bordered table-striped dataTable_camilo" width="100%">
             <tfoot>
                 <tr>
                     <th></th>
