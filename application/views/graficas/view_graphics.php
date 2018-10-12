9e678f01ddc76fcb8abee66b8440090df7d331e1
bredybuitrago <bredybuitrago@gmail.com>
2018-10-12 15:20:33 -0500

    final graficas

--- application/views/graficas/view_graphics.php
+++ application/views/graficas/view_graphics.php
@@ -1,37 +1,32 @@
 
 <script src="<?= URL::to('assets/plugins/charjs/chart.min.js'); ?>"></script>
-
-<h2 align="center"><i><img src="<?= URL::to('assets/images/BBVAicon.png') ?>" style="height: 39px; position: fixed; width: 140px; margin: -2.5% -33.6%;"></i>SEGUIMIENTO EFECTIVIDAD</h2><br>
+<i><img src="<?= URL::to('assets/images/BBVAicon.png') ?>" style="height: 39px; position: fixed; width: 140px; margin: 1.5% -5.6%;"></i>
 <div style="background: #d0cfcf63; border: 1px solid; border-radius: 15px; display: flow-root;">
-    <div class="col-md-12" style="margin-top: 50px;">
+	<h2 align="center">SEGUIMIENTO EFECTIVIDAD</h2>
+    <div class="col-md-12" style="margin-top: 20px;">
         <canvas id="barras_1" height="100" style="margin-bottom: 40px"></canvas>
     </div>
 
     <div class="col-md-12" style="margin-top: 50px;">
             <canvas id="torta_1" height="150"></canvas>
     </div>
-    <!--              cominezo jhonChaparro              -->
     <div class="col-md-12" style="margin-top: 50px;">
         <canvas id="barras_2" height="100"></canvas>
     </div>
-    <!--                 fin jhonChaparro                 -->
 </div>
 
-
-<!--****************************************************** seccion 3 CAMILO ******************************************************-->
-
-<!--****************************************************** FIN CAMILO SECCION 3 ******************************************************-->
-<div>
-    <div class="col-md-12" style="margin-top: 50px;">
-        <canvas id="barras_3" height="100"></canvas>
-    </div>
-    <div class="col-md-12" style="margin-top: 50px;">
-        <canvas id="torta_3"></canvas>
-    </div>
-    <!--              cominezo jhonChaparro              -->
-    <div class="col-md-12">
-        <canvas id="barras_4" height="200"></canvas>
-    </div>
-    <!--                 fin jhonChaparro                 -->
+<br><br><br>
+<div style="background: #d0cfcf63; border: 1px solid; border-radius: 15px; display: flow-root;">
+	<h2 align="center">CONSOLIDADO GENERAL EFECTIVIDAD</h2>
+	<div>
+	    <div class="col-md-12" style="margin-top: 50px;">
+	        <canvas id="barras_3" height="100"></canvas>
+	    </div>
+	    <div class="col-md-12" style="margin-top: 50px;">
+	        <canvas id="torta_3"></canvas>
+	    </div>
+	    <div class="col-md-12" style="margin-top: 50px;">
+	        <canvas id="barras_4" height="200"></canvas>
+	    </div>
+	</div>
 </div>
-
