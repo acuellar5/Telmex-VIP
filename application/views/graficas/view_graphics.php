d140f035182524182c8f476e9f3dab74e49d86b5
SatanStriike <johnfbr1998@hotmail.com>
2018-10-12 13:59:29 -0500

    MERGE

--- application/views/graficas/view_graphics.php
+++ application/views/graficas/view_graphics.php
@@ -1,6 +1,6 @@
 <script src="<?= URL::to('assets/plugins/charjs/chart.min.js'); ?>"></script>
 <h1 align="center"><i><img src="<?= URL::to('assets/images/BBVAicon.png') ?>" style="height: 39px; position: fixed; width: 140px; margin: -2.5% -41.6%;"></i>EFECTIVIDAD</h1><br>
-<div class="row">
+<div class="row"> 
 	<div class="col-md-6">
 		
 	</div>
