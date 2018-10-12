3661a4a85ab20838e76051a55312d021900a7fba
bredybuitrago <bredybuitrago@gmail.com>
2018-10-12 13:58:20 -0500

    GREFICAS 68%

--- application/views/graficas/view_graphics.php
+++ application/views/graficas/view_graphics.php
@@ -1,3 +1,4 @@
+
 <script src="<?= URL::to('assets/plugins/charjs/chart.min.js'); ?>"></script>
 
 <h2 align="center"><i><img src="<?= URL::to('assets/images/BBVAicon.png') ?>" style="height: 39px; position: fixed; width: 140px; margin: -2.5% -41.6%;"></i>SEGUIMIENTO EFECTIVIDAD</h2><br>
@@ -14,3 +15,18 @@
 	</div>
 </div>
 
+<!--****************************************************** seccion 3 CAMILO ******************************************************-->
+
+<!--****************************************************** FIN CAMILO SECCION 3 ******************************************************-->
+<div class="">
+	<div class="col-md-12">
+		<div >
+			<canvas id="barras_3" height="100"></canvas>
+		</div>
+	</div>
+	<div class="col-md-15">
+		<div id="contentGraphics" >
+			<canvas id="torta_3"></canvas>
+		</div>
+	</div>
+</div>
